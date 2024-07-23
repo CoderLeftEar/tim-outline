22cc97b66d2a3814796b201b1dba5fe7b56922a9
xuht <xuht@g-bits.com>
2024-07-23 10:44:40 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { computed } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+import { useRoute } from 'vue-router';
+
+const route = useRoute()
+
+const title = computed(() => {
+  return route.query.title
+})
+
+const FIELD_NAMES = {
+  label: 'title',
+  value: 'key'
+}
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <!-- <HelloWorld :field-names="FIELD_NAMES" /> -->
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,92 @@
+<template>
+  <n-data-table
+    :columns="columns"
+    :data="data"
+    :max-height="250"
+    :scroll-x="1800"
+    :render-cell="renderCell"
+    virtual-scroll
+  />
+</template>
+
+<script setup>
+import { h } from "vue";
+import { NDataTable } from "naive-ui";
+
+const renderCell = (value, rowData, column) => {
+  const onMouseover = () => {
+    console.log("onmouseover", value, rowData, column);
+  }
+
+  const onMouseleave = () => {
+    console.log("onmouseleave", value, rowData, column);
+  }
+
+  return h(
+    "span",
+    {
+      onMouseover,
+      onMouseleave,
+      class: "custom-cell"
+    },
+    value
+  )
+}
+
+const columns = [
+  {
+    type: "selection",
+    fixed: "left"
+  },
+  {
+    title: "Name",
+    key: "name",
+    width: 200,
+    fixed: "left"
+  },
+  {
+    title: "Age",
+    key: "age",
+    width: 100,
+    fixed: "left"
+  },
+  {
+    title: "Row",
+    key: "row",
+    render(row, index) {
+      return h("span", ["row ", index]);
+    }
+  },
+  {
+    title: "Row1",
+    key: "row1",
+    render(row, index) {
+      return h("span", ["row ", index]);
+    }
+  },
+  {
+    title: "Row2",
+    key: "row2",
+    render(row, index) {
+      return h("span", ["row ", index]);
+    },
+    width: 100,
+    fixed: "right"
+  },
+  {
+    title: "Address",
+    key: "address",
+    width: 200,
+    fixed: "right"
+  }
+];
+
+const data = Array.from({ length: 5e3 }).map((_, index) => ({
+  key: index,
+  name: `Edward King ${index}`,
+  age: 32,
+  address: `London, Park Lane no. ${index}`
+}));
+
+const key = 'address'
+</script>
